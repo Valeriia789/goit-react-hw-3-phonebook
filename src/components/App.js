323efeafb5d1af97de93966d16f1d5af634a9667
Valeriia <valeriia.air@gmail.com>
2022-08-17 19:21:43 +0300

    page test

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ export default class App extends Component {
     }
   }
 
+  
   addContact = (name, number) => {
     const { contacts } = this.state
     const names = contacts.map(contact => contact.name)
